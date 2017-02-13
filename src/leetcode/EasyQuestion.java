98f4b3f5ce6283667450570d636116ffec679a19
AttackOnJasper <wangdc2010@gmail.com>
2017-02-12 22:06:05 -0500

    init commit

---
+++ src/leetcode/EasyQuestion.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+ * Created by jasperwang on 16/11/12.
+ */
+public class EasyQuestion {
+    public String reverseString(String s){
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    public String reverseString2(String s) {
+        char[] arr = s.toCharArray();
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return arr.toString();
+    }
+
+    public int findComplement(int num) {
+        return (Integer.highestOneBit(num) << 1) - 1 - num;
+    }
+
+
+    public static void main(String[] args) {
+        EasyQuestion eq = new EasyQuestion();
+        System.out.println(eq.reverseString("olleh"));
+        System.out.println(eq.findComplement(5));
+    }
+}
