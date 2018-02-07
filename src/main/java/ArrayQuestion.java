98867925c962e1af16a41a09c9487d71696185de
Daocheng Wang <d93wang@uwaterloo.ca>
2018-02-06 23:55:13 -0500

    added 75

--- src/main/java/ArrayQuestion.java
+++ src/main/java/ArrayQuestion.java
@@ -111,6 +111,30 @@ public class ArrayQuestion {
         }
     }
 
+    // 75 sort array of 0, 1, 2
+    public void sortColors(int[] nums) {
+        if(nums==null || nums.length<2) return;
+        int low = 0;
+        int high = nums.length-1;
+        for(int i = low; i<=high;) {
+            if(nums[i]==0) {
+                // swap A[i] and A[low] and i,low both ++
+                int temp = nums[i];
+                nums[i] = nums[low];
+                nums[low]=temp;
+                i++;low++;
+            }else if(nums[i]==2) {
+                //swap A[i] and A[high] and high--;
+                int temp = nums[i];
+                nums[i] = nums[high];
+                nums[high]=temp;
+                high--;
+            }else {
+                i++;
+            }
+        }
+    }
+
     // 118
     public List<List<Integer>> generatePascal(int n) {
         List<List<Integer>> res = new ArrayList<>();
