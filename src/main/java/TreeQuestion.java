ac83bc808d110e5ddb7318afa66d64ec0d5c2063
Daocheng Wang <d93wang@uwaterloo.ca>
2018-02-09 23:23:31 -0500

    updated 226

--- src/main/java/TreeQuestion.java
+++ src/main/java/TreeQuestion.java
@@ -322,13 +322,28 @@ public class TreeQuestion {
         return prev;
     }
 
-    // 216
+    // 226 Invert BST
+    /** note the use of temp TreeNode to hold the original right */
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
-        TreeNode newLeft = invertTree(root.right);
-        TreeNode newRight = invertTree(root.left);
-        root.left = newLeft;
-        root.right = newRight;
+        TreeNode tempRight = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(tempRight);
+        return root;
+    }
+    /** dfs */
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) return null;
+        final Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            final TreeNode node = stack.pop();
+            final TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+            if(node.left != null) stack.push(node.left);
+            if(node.right != null) stack.push(node.right);
+        }
         return root;
     }
 
