ed4e510731cc5d60c9e9b4e852b647aa9d6edc8f
Daocheng Wang <d93wang@uwaterloo.ca>
2018-03-11 16:04:08 -0400

    changed comment for 479

--- src/main/java/EasyQuestion.java
+++ src/main/java/EasyQuestion.java
@@ -385,9 +385,8 @@ public class EasyQuestion {
         return ~num & ((Integer.highestOneBit(num) << 1) - 1);
     }
 
-    // 479. Largest Palindrome Product
+    // 479. Largest Palindrome Product: return the max palindrome from product of 2 n-digit numbers
     public int largestPalindrome(int n) {
-        // if input is 1 then max is 9
         if(n == 1) return 9;
         // if n = 3 then upperBound = 999 and lowerBound = 99
         int upperBound = (int) Math.pow(10, n) - 1, lowerBound = upperBound / 10;
@@ -408,9 +407,7 @@ public class EasyQuestion {
             // here i and palindrom/i forms the two factor of assumed palindrom
             for (long i = upperBound; upperBound > lowerBound; i--) {
                 // if n= 3 none of the factor of palindrom  can be more than 999 or less than square root of assumed palindrom
-                if (palindrom / i > maxNumber || i * i < palindrom) {
-                    break;
-                }
+                if (palindrom / i > maxNumber || i * i < palindrom) break;
 
                 // if two factors found, where both of them are n-digits,
                 if (palindrom % i == 0) {
